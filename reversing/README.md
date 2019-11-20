97bb19f45d5944a88087107a8137e2c8f01e5484
takeshix <takeshix@adversec.com>
2019-11-20 13:06:49 +0100

    Added binfetch.ps1

--- reversing/README.md
+++ reversing/README.md
@@ -4,6 +4,10 @@
 
 Pack a binary and all it's required shared objects into a tarball. Useful to get all files required to execute a binary.
 
+## [binfetch.ps1](binfetch.ps1)
+
+Windows PowerShell port of [binfetch.sh](binfetch.sh).
+
 ## [KLMViewer_frida.py](KLMViewer_frida.py)
 
 Extract PDF files from KLMviewer.exe after decryption and decompression. See [this blogpost](https://insinuator.net/2018/11/dumping-decrypted-documents-from-a-north-korean-pdf-reader/) for further information how to use it.
---
+++ reversing/binfetch.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [parameter(Mandatory=$true)][string]$inFile,
+    [parameter(Mandatory=$true)][string]$outPath
+)
+$filePath = Resolve-Path $inFile
+$fileName = Split-Path -Path $filePath -Leaf
+$modules = Start-Process -PassThru $filePath | Get-Process -Module 
+echo "Found the following modules:"
+echo ($modules).FileName
+$compress = @{
+    LiteralPath = ($modules).FileName
+    CompressionLevel = "Fastest"
+    DestinationPath = $outPath + $fileName + "_packed.zip"
+}
+Compress-Archive @compress

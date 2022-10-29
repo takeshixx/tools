8949d5730020851077c0eb1c19b587536d0dc38e
takeshix <takeshix@adversec.com>
2022-10-29 21:33:33 +0200

    Fixed shell issues in be_silent.sh

--- net/be_silent.sh
+++ net/be_silent.sh
@@ -2,24 +2,24 @@
 # Temporarily block all outgoing traffic
 # on a given interface. Useful for passive
 # network analysis.
-if [ $# -nq 1 ];then
+if [ $# -ne 1 ];then
     echo "Usage: ${0} [interface]"
 fi
 
 interface=$1
 all_interfaces=$(ls /sys/class/net/)
 
-if ! $(echo $all_interfaces|grep -q $interface);then
+if ! echo "$all_interfaces"|grep -q "$interface";then
     echo "Interface ${interface} not found" >&2
     exit 2
 fi
 
 exit(){
-    iptables -D OUTPUT -o $interface -j DROP
+    iptables -D OUTPUT -o "$interface" -j DROP
 }
 
 trap "exit" EXIT TERM
 
-iptables -I OUTPUT -o $interface -j DROP
+iptables -I OUTPUT -o "$interface" -j DROP
 echo "Press ENTER or CTRL+C to stop this script."
 read
